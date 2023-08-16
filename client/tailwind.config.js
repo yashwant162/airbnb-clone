b1412e974ed5b4ab284cb4c4f0b1696e3df72513
yashwant162 <yashwantsingh162@gmail.com>
2023-08-16 14:50:28 +0530

    fr-nav bar add

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F5385D",
+      },
+    },
   },
   plugins: [],
 };
