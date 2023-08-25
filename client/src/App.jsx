c2e39151912e8b66586b1e243f90d50f32e4de93
yashwant162 <yashwantsingh162@gmail.com>
2023-08-25 10:48:39 +0530

    add register route , axios default url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import Layout from "./Layout";
+import RegisterPage from "./pages/RegisterPage";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://127.0.01:5000";
+axios.defaults.withCredentials = true;
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );
