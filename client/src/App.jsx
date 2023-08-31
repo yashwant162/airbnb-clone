19f09618d7282b4b45378df81fa5384f0c02abff
yashwant162 <yashwantsingh162@gmail.com>
2023-08-31 22:31:02 +0530

    correct axios default url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import axios from "axios";
 import { UserContextProvider } from "./UserContext";
 import AccountPage from "./pages/AccountPage";
 
-axios.defaults.baseURL = "http://127.0.01:5000";
+axios.defaults.baseURL = "http://127.0.0.1:5000";
 axios.defaults.withCredentials = true;
 function App() {
   return (
