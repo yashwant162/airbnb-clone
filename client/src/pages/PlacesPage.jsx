dc4a8baf7170c8cd6b6607c8ea806edb845dda0d
yashwant162 <yashwantsingh162@gmail.com>
2023-08-31 22:31:52 +0530

    add  placesPage file

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Perks from "./Perks";
+import axios from "axios";
 
 export default function PlacesPage()  {
   const {action} = useParams()
@@ -36,6 +37,43 @@ export default function PlacesPage()  {
       )
   }
 
+  
+  async function uploadByLink(ev){
+    console.log("in upload ft")
+    ev.preventDefault()
+    try {
+      const {data:filename} = await axios.post("/api/user/upload-by-link", {link:linkPhotos});
+      alert(`Welcome ${filename},`);
+      console.log("Link " + filename)
+      setAddPhotos(prev => {
+        return [...prev, filename]
+      })
+      
+      setLinkPhotos('')
+      
+    } catch (err) {
+      alert("Something went wrong \n" + err.response.data.message);
+    }
+  }
+  
+  async function uploadPhotos(ev){
+
+    const files = ev.target.files
+    const data = new FormData()
+    for(let i = 0; i < files.length; i++){
+      data.append('photos',files[i])
+      console.log(files[i])
+    }
+    console.log("what is the data :" + data)
+
+    const {data:filenames} = await axios.post("/api/user/upload",data,{headers:{'Content-type':'multipart/form-data'}})
+    
+    setAddPhotos(prev => {
+      return [...prev, ...filenames]
+    })
+  
+  }
+  
   return (
     <div>
       {action !== "new" && (
@@ -50,63 +88,63 @@ export default function PlacesPage()  {
         <div>
           <form >
             {preInput('Title','Title for your place, should be short and catchy as in advertisement')}
-            <input type="text" placeholder="title, for example: My Hotel "/>
+            <input type="text" value={title} onChange={ev => setTitle(ev.target.value)} placeholder="title, for example: My Hotel "/>
 
-            <h2 className="text-2xl mt-4">Address</h2>
-            <p className="text-gray-500 text-sm">Address to your place</p>
-            <input type="text" placeholder="your address"/>
+            {preInput('Address','Address to your place')}
+            <input type="text" value={address} onChange={ev => setAddress(ev.target.value)} placeholder="your address"/>
 
-            <h2 className="text-2xl mt-4">Photos</h2>
-            <p className="text-gray-500 text-sm">more is better</p>
-
+            {preInput('Photos','more is better')}
             <div className="flex gap-2">
-              <input type="text" placeholder="Add using links...jpg" />
-              <button className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
+              <input type="text" value={linkPhotos} onChange={ev => setLinkPhotos(ev.target.value)} placeholder="Add using links...jpg" />
+              <button  onClick={uploadByLink}  className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
             </div>
-
-            <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-              <button className="justify-center flex gap-1 border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
+            
+            <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-2">
+              {addPhotos.length > 0 && addPhotos.map(link => (
+                <div className="h-32 flex" key = {link}>
+                  <img className="rounded-2xl w-full object-cover" src={"http://localhost:5000/uploads/"+link} alt="" />
+                </div>
+              ))} 
+              
+              <label className=" cursor-pointer items-center justify-center flex gap-1 border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
+              <input type="file" multiple className="hidden" onChange={uploadPhotos}/>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8"><path strokeLinecap="round" strokeLinejoin="round" d="M12 9.75v6.75m0 0l-3-3m3 3l3-3m-8.25 6a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" /></svg>
                 Upload
-              </button>
+              </label>
             </div>
 
-            <h2 className="text-2xl mt-4">Description</h2>
-            <p className="text-gray-500 text-sm">description for your place</p>
-            <textarea/>
+            {preInput('Description','description for your place')}
+            <textarea value={description} onChange={ev => setDescription(ev.target.value)}/>
 
-            <h2 className="text-2xl mt-4">Perks</h2>
-            <p className="text-gray-500 text-sm">select all the perks</p>
-
+            {preInput('Perks','select all the perks')}
             <div className="grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6 mt-2">
-            <Perks selected={perks} onChange={setPerks}/>
+              <Perks selected={perks} onChange={setPerks}/>
             </div>
 
-            <h2 className="text-2xl mt-4">Extra Info</h2>
-            <p className="text-gray-500 text-sm">House Rules and other stuffs</p>
-            <textarea/>
+            {preInput('Extra Info','House Rules and other stuffs')}
+            <textarea value={extraInfo} onChange={ev => setExtraInfo(ev.target.value)} />
 
-            <h2 className="text-2xl mt-4">Check In & Out </h2>
-            <p className="text-gray-500 text-sm">add check in and out times.</p>
-
+            {preInput('Check In & Out','add check in and out times.')}
             <div className="grid gap-2 sm:grid-cols-3">
               <div>
                 <h3 className="mt-2 -mb-1">Check In</h3>
-                <input type="text" placeholder="14:00"/>
+                <input type="text" value={checkIn} onChange={ev => setCheckIn(ev.target.value)} placeholder="14:00"/>
               </div>
               <div>
                 <h3 className="mt-2 -mb-1">Check Out</h3>   
-                <input type="text" placeholder="14:00"/>
+                <input type="text" value={checkOut} onChange={ev => setCheckOut(ev.target.value)} placeholder="11:00"/>
               </div>
               <div>
                 <h3 className="mt-2 -mb-1">Max Guests</h3>  
-                <input type="text" placeholder="3 Persons"/>
+                <input type="number" value={maxGuests} onChange={ev => setMaxGuests(ev.target.value)} placeholder="3 Persons"/>
               </div>
             </div>
 
             <button className="primary my-4">Save</button>
 
           </form>
+          
+
         </div>
 
       )}
