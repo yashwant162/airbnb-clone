437d32adb4f6e2d55a4210ea34d58ed8d70cdfcb
yashwant162 <yashwantsingh162@gmail.com>
2023-09-08 12:10:22 +0530

    add data routes for places

--- api/routes/dataRoutes.js
+++ api/routes/dataRoutes.js
@@ -2,14 +2,18 @@ const asyncHandler = require('express-async-handler')
 const multer = require('multer')
 const photosMiddleware = multer({dest:'uploads'})
 const { validateToken } = require('../middleware/validateTokenHandler')
-const { getPlaces, uploadPhotos, addPlace } = require('../controllers/dataController');
+const { getUserPlaces, uploadPhotos, addPlace, getPlaceById, updatePlace, getAllPlaces } = require('../controllers/dataController');
 const { uploadByLink } = require('../controllers/dataController');
 
 const router = require('express').Router()
 
-router.post("/upload-by-link", asyncHandler(uploadByLink));
-router.post("/upload",photosMiddleware.array('photos',100) ,uploadPhotos);
-router.post("/add-place",asyncHandler(validateToken), asyncHandler(addPlace));
-router.get('/get-places',asyncHandler(validateToken),asyncHandler(getPlaces))
+router.post("/upload-by-link", asyncHandler(uploadByLink))
+router.post("/upload",photosMiddleware.array('photos',100) ,uploadPhotos)
+router.post("/add-place",asyncHandler(validateToken), asyncHandler(addPlace))
+router.put("/add-place",asyncHandler(validateToken), asyncHandler(updatePlace))
+router.get('/get-user-places',asyncHandler(validateToken),asyncHandler(getUserPlaces))
+router.get('/get-all-places',asyncHandler(validateToken),asyncHandler(getAllPlaces))
+router.get('/place/:id',asyncHandler(validateToken),asyncHandler(getPlaceById))
+
 
 module.exports = router
