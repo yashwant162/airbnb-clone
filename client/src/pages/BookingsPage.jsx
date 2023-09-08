cf305447e7917f91636b9d680d9b20dc16fe9da9
yashwant162 <yashwantsingh162@gmail.com>
2023-09-08 12:12:11 +0530

    add booking page no data for now

---
+++ client/src/pages/BookingsPage.jsx
@@ -0,0 +1,12 @@
+import AccountNav from "../AccountNav"
+
+export default function BookingsPage(){
+  return(
+    <div className="">
+      <AccountNav/>
+      <div className="text-center mt-12">
+        <h2 className="text-5xl">Booking Page</h2>
+      </div>
+    </div>
+  )
+}
