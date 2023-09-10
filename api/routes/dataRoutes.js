7b67ef3b4acfb4ab3d1d18eb442193a076a5f3f9
yashwant162 <yashwantsingh162@gmail.com>
2023-09-11 01:07:48 +0530

    change api name for get user place by id and add view page api

--- api/routes/dataRoutes.js
+++ api/routes/dataRoutes.js
@@ -2,7 +2,7 @@ const asyncHandler = require('express-async-handler')
 const multer = require('multer')
 const photosMiddleware = multer({dest:'uploads'})
 const { validateToken } = require('../middleware/validateTokenHandler')
-const { getUserPlaces, uploadPhotos, addPlace, getPlaceById, updatePlace, getAllPlaces } = require('../controllers/dataController');
+const { getUserPlaces, uploadPhotos, addPlace, updatePlace, getAllPlaces,getPlaceById } = require('../controllers/dataController');
 const { uploadByLink } = require('../controllers/dataController');
 
 const router = require('express').Router()
@@ -12,8 +12,9 @@ router.post("/upload",photosMiddleware.array('photos',100) ,uploadPhotos)
 router.post("/add-place",asyncHandler(validateToken), asyncHandler(addPlace))
 router.put("/add-place",asyncHandler(validateToken), asyncHandler(updatePlace))
 router.get('/get-user-places',asyncHandler(validateToken),asyncHandler(getUserPlaces))
-router.get('/get-all-places',asyncHandler(validateToken),asyncHandler(getAllPlaces))
-router.get('/place/:id',asyncHandler(validateToken),asyncHandler(getPlaceById))
+router.get('/get-all-places',asyncHandler(getAllPlaces))
+router.get('/get-user-place-by-id/:id',asyncHandler(validateToken),asyncHandler(getPlaceById))
+router.get('/place/:id',asyncHandler(getPlaceById))
 
 
 module.exports = router
