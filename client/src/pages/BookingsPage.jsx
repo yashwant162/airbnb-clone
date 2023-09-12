f36b3fa1a64fbe23673db6c39619b1a25394db81
yashwant162 <yashwantsingh162@gmail.com>
2023-09-12 23:48:35 +0530

    add booking Page

--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -1,11 +1,65 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react"
 import AccountNav from "../AccountNav"
+import axios from "axios"
+import { differenceInCalendarDays, format } from "date-fns"
+import { Link } from "react-router-dom"
 
 export default function BookingsPage(){
+  const [bookings, setBookings] = useState([])
+  useEffect(() => {
+    
+    async function fetchBookings(){
+      const {"data": {bookingsData}} = await axios.get("api/data/get-user-bookings")
+      console.log(bookingsData)
+      setBookings(bookingsData)
+    }
+    
+     
+    fetchBookings()
+  },[])
   return(
     <div className="">
       <AccountNav/>
-      <div className="text-center mt-12">
-        <h2 className="text-5xl">Booking Page</h2>
+      <div className="">
+        {bookings?.length > 0 && bookings.map(booking => (
+          <Link to={`/account/bookings/${booking._id}`} className="flex gap-4 bg-gray-200 rounded-2xl overflow-hidden mt-4" key={booking.place}>
+            <div className="w-48">
+              <img className="object-cover" src={"http://localhost:5000/uploads/"+booking.place.photos[0]} alt="" />
+            </div>
+            
+            <div className="py-3 grow pr-3">
+              <h2 className="text-xl">{booking.place.title}</h2>
+              <div className="text-xl">
+                <div className="flex gap-1 mb-2 mt-4 text-sm text-gray-500">
+                  
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" /></svg>
+                  {differenceInCalendarDays(new Date(booking.checkOut),new Date(booking.checkIn))} Nights
+                  
+                  <div className="flex gap-2 items-center ml-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" /></svg>
+                    {format(new Date(booking.checkIn),'MMMM-dd-yyyy')}
+                  </div>
+
+                  &rarr; 
+                  
+                  <div className="flex gap-2 items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" /></svg>
+                    {format(new Date(booking.checkOut),'MMMM-dd-yyyy')}
+                  </div>
+
+                </div>
+
+                <div className="flex">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-6"><path strokeLinecap="round" strokeLinejoin="round" d="M15 8.25H9m6 3H9m3 6l-3-3h1.5a3 3 0 100-6M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                  <span className="text-base">
+                    {booking.price}/-
+                  </span>
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   )
