38d7d62d2bf539270ffa13f709755975fe0ceba9
yashwant162 <yashwantsingh162@gmail.com>
2023-09-13 16:22:37 +0530

    add view-booked page

---
+++ client/src/pages/BookedPage.jsx
@@ -0,0 +1,47 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import AddressLink from "../AddressLink"
+import PlaceGallery from "../PlaceGallery"
+import BookingDates from "../BookingDates"
+
+export default function BookedPage(){
+  const {id} = useParams()
+  const [booking, setBooking] = useState('')
+  console.log("id sent is "+id)
+
+  useEffect(() => {
+    console.log("id in useEffect is "+id)
+    const sendData = { BookingId:id }
+    console.log("Send Data is "+ sendData)
+    async function fetchBooking(){
+
+      const {"data":{bookingData}} = await axios.get("api/data/get-booking-by-id",{params: sendData})
+      console.log("Received Data is " + JSON.stringify(bookingData))
+      setBooking(bookingData)
+    }
+    fetchBooking()
+  },[id])
+
+  if(!booking){
+    return ''
+  }
+
+  return (
+    <div className="my-8">
+      <h1 className="text-3xl">{booking.place?.title}</h1>
+      <AddressLink className="my-2">{booking.place.address}</AddressLink>
+      <div className="bg-gray-200 p-6 my-6 rounded-2xl items-center flex justify-between">
+        <div>
+          <h2 className="text-2xl mb-4">Your Booking Information:</h2>
+          <BookingDates booking={booking}/>
+        </div>
+        <div className="bg-primary p-4 text-white rounded-2xl">
+          <div>Total Price</div>
+          <div className="text-3xl">₹{booking.price}</div>
+        </div>
+      </div>
+      <PlaceGallery place = {booking.place}/>
+    </div>
+  )
+}
