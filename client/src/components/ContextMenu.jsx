525087b7c34f103a7a6961fecda2cab648494426
yashwant162 <yashwantsingh162@gmail.com>
2023-09-28 21:00:21 +0530

    add component-context menu

---
+++ client/src/components/ContextMenu.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { useEffect, useRef } from "react"
+
+export default function ContextMenu({options, coordinates, contextMenu, setContextMenu}){
+  const contextMenuRef = useRef(null)
+
+  const handleClick = (e,callBack) => {
+    e.stopPropagation()
+    callBack()
+  }
+
+  useEffect(() => {
+    const handleClickOutsideMenu = (event) => {
+        
+    } 
+  }, [])
+  
+  return (
+    <div
+      className={` bg-white shadow-2xl fixed py-5  z-[100] rounded-lg border border-gray-200`}
+      ref={contextMenuRef}
+      style={{
+        boxShadow:
+          "0 2px 5px 0 rgba(var(11,20,26),.26),0 2px 10px 0 rgba(11,20,26;),.16)",
+        top: coordinates.y,
+        left: coordinates.x,
+      }}
+    >
+      <ul>
+        {options.map(({ name, callBack }) => (
+          <li
+            className="hover:bg-gray-100 pl-5 pr-10  py-2 cursor-pointer"
+            key={name}
+            onClick={(e) => handleClick(e,callBack)}
+          >
+            <span className="">{name}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
