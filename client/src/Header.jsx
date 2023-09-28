a31c665c2b91c0ea5c9bed746707636103a8fe01
yashwant162 <yashwantsingh162@gmail.com>
2023-09-28 20:59:59 +0530

    create separate file for keeping svgs

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,69 +1,96 @@
-import { useContext } from "react";
+/* eslint-disable no-unused-vars */
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "./UserContext";
+import AirBnbLogo, { Globe, HamBurger, SearchLogo, UserLogo } from "../public/SvgComponents";
+import ContextMenu from "./components/ContextMenu";
 
 export default function Header() {
   const {user} = useContext(UserContext)
+  const [isContextMenuVisible, setIsContextMenuVisible] = useState(false)
+  const contextMenuOptions = [
+    {
+      name: "Login",
+      callBack: () => {
+        setIsContextMenuVisible(false)
+      },
+    },
+    {
+      name: "Sign Up",
+      callBack: () => {
+        setIsContextMenuVisible(false)
+      },
+    },
+    {
+      name: "Airbnb your home",
+      callBack: () => {
+        setIsContextMenuVisible(false)
+      },
+    },
+    {
+      name: "Help",
+      callBack: () => {
+        setIsContextMenuVisible(false)
+      },
+    },
+  ]
+
+
   console.log(user)
   return (
-    <header className="flex justify-between">
+    <header className="w-full flex flex-row justify-between transition-all duration-300 h-15 border-b border-b-gray-200 pb-4">
+        <Link to={"/"} className="flex items-center gap-1">
+          <AirBnbLogo/>
+          {/* <span className="font-bold text-xl">airbnb</span> */}
+        </Link>
 
-      <Link to={"/"} className="flex items-center gap-1">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6 -rotate-90"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-          />
-        </svg>
-        <span className="font-bold text-xl">airbnb</span>
-      </Link>
-
-      <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
-        <div>Anywhere</div>
-        <div className="border-l border-gray-300"></div>
-        <div>Any week</div>
-        <div className="border-l border-gray-300"></div>
-        <div>Add guests</div>
-        <button className="bg-primary text-white p-1 rounded-full">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-4 h-4"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-            />
-          </svg>
-        </button>
-      </div>
-      <Link to={user?'/account':'/login'} className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 ">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-        </svg>
-        <div className="bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 relative top-1">
-            <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
-          </svg>
+        <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
+          <div>Anywhere</div>
+          <div className="border-l border-gray-300"></div>
+          <div>Any week</div>
+          <div className="border-l border-gray-300"></div>
+          <div>Add guests</div>
+          <button className="bg-primary text-white p-1 rounded-full">
+            <SearchLogo/> 
+          </button>
         </div>
-        {!!user && (
-          <div>
-            {user.name}
-          </div>
-        )}
-      </Link>
+
+        <div>
+          <ul className="flex items-center mt-2 gap-2 justify-end font-medium">
+              <li className="cursor-pointer">
+                <span>Airbnb your home</span>
+              </li>
+              <li className="cursor-pointer">
+                <Globe/>
+              </li>
+          </ul>
+        </div>
+
+        <div 
+             className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 hover:shadow-xl transition-all duration-500"
+             onClick={() => setIsContextMenuVisible(true)}     
+          >
+          <Link to={user?'/account':'/login'} className="bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden">
+            <UserLogo/>
+          </Link>
+          <HamBurger/>
+          {!!user && (
+            <div>
+              {user.name}
+            </div>
+          )}
+          {isContextMenuVisible && (
+            <ContextMenu options = {contextMenuOptions}
+                         coordinates= {{
+                          x: window.innerWidth - 250,
+                          y: 70
+                         }}
+                         contextMenu = {isContextMenuVisible}
+                         setContextMenu = {setIsContextMenuVisible}
+            />
+          )}
+        </div>
+
     </header>
   );
 }
