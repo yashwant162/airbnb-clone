805fe48ec3b61ff8608adc0275113367b5eed64d
yashwant162 <yashwantsingh162@gmail.com>
2023-10-01 15:16:45 +0530

    change search svg

--- client/public/SvgComponents.jsx
+++ client/public/SvgComponents.jsx
@@ -40,20 +40,10 @@ export function UserLogo() {
 
 export function SearchLogo(){
   return (
-    <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-4 h-4 text-white"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-              />
-            </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+  <path fillRule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clipRule="evenodd" />
+</svg>
+
   )
 }
 
